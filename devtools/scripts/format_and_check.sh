1489a61cd143448b8e340a2fea5caa868407cc29
Charlles Abreu <charlles@redesignscience.com>
2023-10-12 12:21:29 -0400

    Revert "Initial commit"
    This reverts commit 80cbd4faf0caad9ef688178728b68e3f49ba3bc0.

--- devtools/scripts/format_and_check.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e -v
-isort ./sparsembar
-black --line-length 100 ./sparsembar
-flake8 --ignore=E203,W503 ./sparsembar
-pylint --rcfile=devtools/linters/pylintrc ./sparsembar
--- devtools/scripts/run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-pytest -v -s --cov=sparsembar --cov-report=term-missing --cov-report=html --pyargs --doctest-modules "$@" sparsembar
