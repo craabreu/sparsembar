52e6be4e5de19790233252cb1155275e3c3f5f46
Charlles Abreu <charlles@redesignscience.com>
2023-10-12 13:50:22 -0400

    Added first python files

---
+++ sparsembar/__init__.py
@@ -0,0 +1,7 @@
+"""
+SparseMBAR: A sparse-matrix variant of the multistate Bennett acceptance ratio (MBAR)
+estimator
+"""
+
+# flake8: noqa
+from ._version import __version__
---
+++ sparsembar/sparsembar.py
@@ -0,0 +1,17 @@
+"""
+SparseMBAR: A sparse-matrix variant of the multistate Bennett acceptance ratio (MBAR)
+"""
+
+class SparseMBAR:
+    """
+    A class for performing MBAR estimation on a set of samples from multiple
+    states.
+
+    Examples
+    --------
+    >>> import sparsembar as smbar
+    >>> smbar.__version__
+    '0.0.0'
+    """
+    def __init__(self) -> None:
+        pass
